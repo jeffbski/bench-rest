3bb9ba72525217fc6d3c026e01a879bc8a7f992e
Jeff Barczewski <jeff.barczewski@gmail.com>
2013-01-08 13:17:11 -0600

    document remaining features and add tests

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Simple single GET flow performing 100 requests with 10 concurrent connections
     .on('error', function (err) { console.error(err); })
     .on('end', function (stats, errorCount) {
       console.log('error count: ', errorCount);
-      console.log('stats', stats.toJSON());
+      console.log('stats', stats);
     });
 ```
 
@@ -55,9 +55,103 @@ Advanced flow with setup/teardown and multiple steps to benchmark in each iterat
     .on('error', function (err) { console.error(err); })
     .on('end', function (stats, errorCount) {
       console.log('error count: ', errorCount);
-      console.log('stats', stats.toJSON());
+      console.log('stats', stats);
     });
 ```
+
+### Run options
+
+The runOptions object can have the following properties which govern the benchmark run:
+
+ - `limit` - required number of concurrent operations to limit at any given time
+ - `requests` - required number of flow iterations to perform on the `main` flow (as well as `beforeMain` and `afterMain` setup/teardown operations)
+ - `user` - optional user to be used for basic authentication
+ - `password` - optional password to be used for basic authentication
+
+### REST Operations in the flow
+
+The REST operations that need to be performed in either as part of the main flow or for setup and teardown are configured using the following flow properties.
+
+Each array of opertions will be performed in series one after another unless an error is hit. The afterMain and after operations will be performed regardless of any errors encountered in the flow.
+
+```javascript
+  var flow = {
+    before: [],      // REST operations to perform before anything starts
+    beforeMain: [],  // REST operations to perform before each iteration
+    main: [],        // REST operations to perform for each iteration
+    afterMain: [],   // REST operations to perform after each iteration
+    after: []        // REST operations to perform after everything is finished
+  };
+```
+
+Each operation can have the following properties:
+
+ - one of these common REST properties `get`, `head`, `put`, `post`, `del` (using del rather than delete since delete is a JS reserved word) with a value pointing to the URI, ex: `{ get: 'http://localhost:8000/foo' }`
+ - alternatively can specify `method` (use uppercase) and `uri` directly, ex: `{ method: 'GET', uri: 'http://localhost:8000/foo' }`
+ - `json` optionally provide data which will be JSON stringified and provided as body also setting content type to application/json, ex: `{ put: 'http://localhost:8000/foo', json: { foo: 10 } }`
+ - `headers` - optional headers to set, ex: `{ get: 'http://localhost:8000/foo', headers: { 'Accept-Encoding': 'gzip'}`
+ - any other properties/options which are valid for `mikeal/request` - see https://github.com/mikeal/request
+ - pre/post processing - optional array as `beforeHooks` and `afterHooks` which can perform processing before and/or after an operation. See `Pre/post operation processing` section below for details.
+
+### Token substitution for iteration operations
+
+To make REST flows that are independent of each other, one often wants unique URLs and unique data, so one way to make this easy is to include special tokens in the `uri`, `json`, or `data`.
+
+Currently the token(s) replaced in the `uri`, `json`, or `data` are:
+
+ - `#{INDEX}` - replaced with the zero based counter/index of the iteration
+
+Note: for the `json` property the `json` object is JSON.stringified, tokens substituted, then JSON.parsed back to an object so that tokens will be substituted anywhere in the structure.
+
+
+### Pre/post operation processing
+
+If an array of hooks is specified in an operation as `beforeHooks` and/or `afterHooks` then these synchronous operations will be done before/after the REST operation.
+
+Built-in processing filters can be referred to by name using a string, while custom filters can be provided as a function, ex:
+
+```javascript
+// This causes the HEAD operation to use a previously saved etag if found for this URI
+// setting the If-None-Match header with it, and then if the HEAD request returns a failing
+// status code
+{ head: 'http://localhost:8000', beforeHooks: ['useEtag'], afterHooks: ['ignoreStatus'] }
+```
+
+The list of current built-in beforeHooks:
+
+- `useEtag` - if an etag had been previously saved for this URI with `saveEtag` afterHook, then set the appropriate header (for GET/HEAD, `If-None-Match`, otherwise `If-Match`). If was not previously saved or empty then no header is set.
+
+The list of current built-in afterHooks:
+
+ - `saveEtag` - afterHook which causes an etag to be saved into an object cache specific to this iteration. Stored by URI. If the etag was the result of a POST operation and a `Location` header was provided, then the URI at the `Location` will be used.
+ - `ignoreStatus` - afterHookif an operation could possibly return an error code that you want to ignore and always continue anyway. Failing status codes are those that are greater than or equal to 400. Normal operation would be to terminate an iteration if there is a failure status code in any `before`, `beforeMain`, or `main` operation.
+
+
+To create custom beforeHook or afterHook the synchronous function needs to accept an `all` object and return the same or possibly modified object. To exit the flow, an exception can be thrown which will be caught and emitted.
+
+So a verification function could be written as such
+
+```javascript
+function verifyData(all) {
+  if (all.err) return all; // errored so just return and it will error as normal
+  assert.equal(all.response.statusCode, 200);
+  assert(all.body, 'foobarbaz');
+  return all;
+}
+```
+
+The properties available on the `all` object are:
+
+ - all.env.index - the zero based counter for this iteration, same as what is used for #{INDEX}
+ - all.env.jar - the cookie jar
+ - all.env.user - basic auth user if provided
+ - all.env.password - basic auth password if provided
+ - all.env.etags - object of etags saved by URI
+ - all.opIndex - zero based index for the operation in the array of operations, ie: first operation in the main flow will have opIndex of 0
+ - all.action.requestOptions - the options that will be used for the request
+ - all.err - not empty if an error has occurred
+ - all.cb - the cb that will be called when done
+
 
 ## Goals
 
@@ -66,6 +160,12 @@ Advanced flow with setup/teardown and multiple steps to benchmark in each iterat
  - Obtain metrics from the runs with average, total, min, max, histogram, req/s
  - Allow iterations to vary easily using token subsitution
  - Run programmatically so can be used with CI server
+ - Flow can have setup and teardown operations for startup and shutdown as well as for each iteration
+ - Ability to automatically handles cookies separately for each iteration
+ - Abilty to automatically follows redirects for operations
+ - Errors will automatically stop an iterations flow and be tracked
+ - Easy use and handling of etags
+ - Allows pre/post processing or verification of data
 
 ## Why
 
@@ -96,6 +196,12 @@ ulimit -S -n       # display soft max open files, default 256
 ulimit -H -n       # display hard max open files, default unlimited
 ulimit -S -n 20000  # set soft max open files
 ```
+
+## Dependencies
+
+ - request - https://github.com/mikeal/request - for http/https operations with cookies, redirects
+ - async - https://github.com/caolan/async - for limiting concurrency
+ - measured - https://github.com/felixge/node-measured - for metrics
 
 ## Get involved
 
--- test/bench-rest.mocha.js
+++ test/bench-rest.mocha.js
@@ -2,18 +2,27 @@
 
 var chai = require('chai');
 var http = require('http');
+var accum = require('accum');
 var benchrest = require('../'); // require('bench-rest');
 var t = chai.assert;
 var httpServer;
+var requests = []; // array for tracking requests to http server
 
 suite('bench-rest');
 
 before(function (done) {
   // Start an HTTP server
   httpServer = http.createServer(function (request, response) {
-    // Every request gets the same "Hello Connect" response.
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World");
+    request.pipe(accum.string('utf8', function (str) { // accumululate any incoming data
+      requests.push({ method: request.method, url: request.url, data: str }); // save these
+    }));
+    if (request.url === '/makeError') { // create an unauthorized 401 error for this URL only
+      response.writeHead(401, {"Content-Type": "text/plain"});
+      response.end('Unauthorized');
+    } else { // all other requests get 200 success with Hello World
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.end("Hello World");
+    }
   }).listen(8000);
   done();
 });
@@ -22,7 +31,7 @@ after(function (done) {
   httpServer.close(function () { done(); });
 });
 
-test('simple', function (done) {
+test('simple get', function (done) {
   var flow = {
     main: [{ get: 'http://localhost:8000' }]
   };
@@ -31,10 +40,200 @@ test('simple', function (done) {
     requests: 100
   };
   var errors = [];
+  requests.length = 0;
   benchrest(flow, runOptions)
     .on('error', function (err) { errors.push(err); })
     .on('end', function (stats, errorCount) {
       if (errorCount) done(errors[0] || 'unknown error');
+      t.equal(requests.length, runOptions.requests);
       done();
     });
 });
+
+test('stats provides measured data with totalElapsed and main metrics', function (done) {
+  var flow = {
+    main: [{ get: 'http://localhost:8000' }]
+  };
+  var runOptions = {
+    limit: 10,
+    requests: 100
+  };
+  var errors = [];
+  requests.length = 0;
+  benchrest(flow, runOptions)
+    .on('error', function (err) { errors.push(err); })
+    .on('end', function (stats, errorCount) {
+      if (errorCount) done(errors[0] || 'unknown error');
+      t.isNumber(stats.totalElapsed, 'should have total elapsed time in millisecs');
+      t.equal(stats.main.meter.count, runOptions.requests, 'should have count equal to the requests made');
+      t.isNumber(stats.main.meter.mean, 'should have an average for iterations/sec');
+      t.isNumber(stats.main.histogram.min, 'should have a min time in milliseconds for all iterations');
+      t.isNumber(stats.main.histogram.max, 'should have a max time in milliseconds for all iterations');
+      t.isNumber(stats.main.histogram.sum, 'should have a sum time in milliseconds for all iterations');
+      t.isNumber(stats.main.histogram.mean, 'should have a mean time in milliseconds for all iterations');
+      t.isNumber(stats.main.histogram.p95, 'should have a 95 percentile time in milliseconds for all iterations');
+      done();
+    });
+});
+
+test('simple put/get flow', function (done) {
+  var flow = {
+    main: [
+      { put: 'http://localhost:8000/foo', json: 'mydata' },
+      { get: 'http://localhost:8000/foo' }
+    ]
+  };
+  var runOptions = {
+    limit: 1,   // limiting to single at a time so can guarantee order for test verification
+    requests: 2
+  };
+  var errors = [];
+  requests.length = 0;
+  benchrest(flow, runOptions)
+    .on('error', function (err) { errors.push(err); })
+    .on('end', function (stats, errorCount) {
+      if (errorCount) done(errors[0] || 'unknown error');
+      t.equal(requests.length, runOptions.requests * flow.main.length);
+      t.deepEqual(requests[0], { method: 'PUT', url: '/foo', data: '"mydata"' });
+      t.deepEqual(requests[1], { method: 'GET', url: '/foo', data: '' });
+      t.deepEqual(requests[2], { method: 'PUT', url: '/foo', data: '"mydata"' });
+      t.deepEqual(requests[3], { method: 'GET', url: '/foo', data: '' });
+      done();
+    });
+});
+
+test('put/get flow with token substitution', function (done) {
+  var flow = {
+    main: [
+      { put: 'http://localhost:8000/foo_#{INDEX}', json: 'mydata_#{INDEX}' },
+      { get: 'http://localhost:8000/foo_#{INDEX}' }
+    ]
+  };
+  var runOptions = {
+    limit: 1,   // limiting to single at a time so can guarantee order for test verification
+    requests: 2
+  };
+  var errors = [];
+  requests.length = 0;
+  benchrest(flow, runOptions)
+    .on('error', function (err) { errors.push(err); })
+    .on('end', function (stats, errorCount) {
+      if (errorCount) done(errors[0] || 'unknown error');
+      t.equal(requests.length, runOptions.requests * flow.main.length);
+      t.deepEqual(requests[0], { method: 'PUT', url: '/foo_0', data: '"mydata_0"' });
+      t.deepEqual(requests[1], { method: 'GET', url: '/foo_0', data: '' });
+      t.deepEqual(requests[2], { method: 'PUT', url: '/foo_1', data: '"mydata_1"' });
+      t.deepEqual(requests[3], { method: 'GET', url: '/foo_1', data: '' });
+      done();
+    });
+});
+
+test('put/get flow with before, beforeMain, afterMain, after', function (done) {
+  var flow = {
+    before: [{ head: 'http://localhost:8000/beforeEverything' }],
+    beforeMain: [{ head: 'http://localhost:8000/foo_#{INDEX}?beforeEachIteration' }],
+    main: [
+      { put: 'http://localhost:8000/foo_#{INDEX}', json: 'mydata_#{INDEX}' },
+      { get: 'http://localhost:8000/foo_#{INDEX}' }
+    ],
+    afterMain: [{ del: 'http://localhost:8000/foo_#{INDEX}?afterEachIteration' }],
+    after: [{ head: 'http://localhost:8000/afterEverything' }]
+  };
+  var runOptions = {
+    limit: 1,   // limiting to single at a time so can guarantee order for test verification
+    requests: 2
+  };
+  var errors = [];
+  requests.length = 0;
+  benchrest(flow, runOptions)
+    .on('error', function (err) { errors.push(err); })
+    .on('end', function (stats, errorCount) {
+      if (errorCount) done(errors[0] || 'unknown error');
+      var totalRequests = runOptions.requests *
+        (flow.main.length + flow.beforeMain.length + flow.afterMain.length) +
+        flow.before.length + flow.after.length;
+      t.equal(requests.length, totalRequests);
+      t.deepEqual(requests[0], { method: 'HEAD', url: '/beforeEverything', data: '' });
+      t.deepEqual(requests[1], { method: 'HEAD', url: '/foo_0?beforeEachIteration', data: '' });
+      t.deepEqual(requests[2], { method: 'PUT', url: '/foo_0', data: '"mydata_0"' });
+      t.deepEqual(requests[3], { method: 'GET', url: '/foo_0', data: '' });
+      t.deepEqual(requests[4], { method: 'DELETE', url: '/foo_0?afterEachIteration', data: '' });
+      t.deepEqual(requests[5], { method: 'HEAD', url: '/foo_1?beforeEachIteration', data: '' });
+      t.deepEqual(requests[6], { method: 'PUT', url: '/foo_1', data: '"mydata_1"' });
+      t.deepEqual(requests[7], { method: 'GET', url: '/foo_1', data: '' });
+      t.deepEqual(requests[8], { method: 'DELETE', url: '/foo_1?afterEachIteration', data: '' });
+      t.deepEqual(requests[9], { method: 'HEAD', url: '/afterEverything', data: '' });
+      done();
+    });
+});
+
+test('errors should be emitted and errorCount should return total', function (done) {
+  var flow = {
+    main: [
+      { get: 'http://localhost:8000/foo' },
+      { put: 'http://localhost:8000/makeError', json: 'mydata' }
+    ]
+  };
+  var runOptions = {
+    limit: 2,
+    requests: 2
+  };
+  var errors = [];
+  requests.length = 0;
+  benchrest(flow, runOptions)
+    .on('error', function (err) { errors.push(err); })
+    .on('end', function (stats, errorCount) {
+      t.equal(errors.length, runOptions.requests, 'should have one error per iteration');
+      t.match(errors[0].message, /401/);
+      t.match(errors[1].message, /401/);
+      done();
+    });
+});
+
+
+test('missing requests property throws error', function () {
+  var flow = {
+    main: [{ get: 'http://localhost:8000' }]
+  };
+  var runOptions = {
+    limit: 10,
+    // requests: 100
+  };
+  function runWhichThrows() {
+    benchrest(flow, runOptions);
+  }
+  t.throws(runWhichThrows, /benchmark runOptions requires requests and limit properties/,
+           'should throw when missing required property runOptions.requests');
+});
+
+test('missing limit property throws error', function () {
+  var flow = {
+    main: [{ get: 'http://localhost:8000' }]
+  };
+  var runOptions = {
+    // limit: 10,
+    requests: 100
+  };
+  function runWhichThrows() {
+    benchrest(flow, runOptions);
+  }
+  t.throws(runWhichThrows, /benchmark runOptions requires requests and limit properties/,
+           'should throw when missing required property runOptions.limit');
+});
+
+test('missing main flow throws error', function () {
+  var flow = {
+    // main: [{ get: 'http://localhost:8000' }]
+  };
+  var runOptions = {
+    limit: 10,
+    requests: 100
+  };
+  function runWhichThrows() {
+    benchrest(flow, runOptions);
+  }
+  t.throws(runWhichThrows, /benchmark flow requires an array of operations as property main/,
+           'should throw when missing flow.main');
+
+});
+
